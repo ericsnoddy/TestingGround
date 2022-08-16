a54930151fddd80c6368fcce81b3e8ee56c0f31f
ericsnoddy <ericsnoddy@gmail.com>
2022-08-16 10:18:46 -0400

    improved debug printer - multi-line output

--- Mouse/debug.py
+++ Mouse/debug.py
@@ -8,14 +8,15 @@ TOPLEFT = (10,10)
 FONT = None  # None for pygame default font
 ANTIALIAS = True
 
-# This method converts the input to a string and displays it
+# This method converts the input list to a string and displays it
 # as a rect at position topleft = (x,y), default included
+# Must input a list (single strings get chopped)
 
-f = pygame.font.Font(FONT,30)
-line_height = pygame.font.Font.get_linesize(f)
-
-def debug(info_list, x = TOPLEFT[0], y = TOPLEFT[1]):    
+def debug(info_list, x = TOPLEFT[0], y = TOPLEFT[1]):
+    f = pygame.font.Font(FONT,30)  
     display_surf = pygame.display.get_surface()
+    line_height = pygame.font.Font.get_linesize(f)
+    print(line_height)
 
     for index, line in enumerate(info_list):
         debug_surf = f.render(str(line), ANTIALIAS, FONT_COLOR)
