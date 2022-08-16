4615365554581a1f163a9f7ef97d6b3369a953a9
ericsnoddy <ericsnoddy@gmail.com>
2022-08-16 10:13:25 -0400

    improved debug printer to accept multi-line list

--- Mouse/debug.py
+++ Mouse/debug.py
@@ -4,7 +4,7 @@ pygame.init()
 # Attributes
 BG_COLOR = (0,0,0)
 FONT_COLOR = (255,255,255)
-DEFAULT_X, DEFAULT_Y = (10,10)
+TOPLEFT = (10,10)
 FONT = None  # None for pygame default font
 ANTIALIAS = True
 
@@ -12,10 +12,14 @@ ANTIALIAS = True
 # as a rect at position topleft = (x,y), default included
 
 f = pygame.font.Font(FONT,30)
+line_height = pygame.font.Font.get_linesize(f)
 
-def debug(info, x = DEFAULT_X, y = DEFAULT_Y):    
+def debug(info_list, x = TOPLEFT[0], y = TOPLEFT[1]):    
     display_surf = pygame.display.get_surface()
-    debug_surf = f.render(str(info), ANTIALIAS, FONT_COLOR)
-    debug_rect = debug_surf.get_rect(topleft = (x,y))
-    pygame.draw.rect(display_surf, BG_COLOR, debug_rect)
-    display_surf.blit(debug_surf, debug_rect)
+
+    for index, line in enumerate(info_list):
+        debug_surf = f.render(str(line), ANTIALIAS, FONT_COLOR)
+        position = (20, TOPLEFT[1] + index * line_height)
+        debug_rect = debug_surf.get_rect(topleft = position)
+        pygame.draw.rect(display_surf, BG_COLOR, debug_rect)
+        display_surf.blit(debug_surf, debug_rect)
