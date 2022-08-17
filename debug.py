aa952f89b2b2f53d4e484d7025b95848198e4904
ericsnoddy <ericsnoddy@gmail.com>
2022-08-17 14:17:13 -0400

    Dunno why inflate breaks program I FUCKING GIVE UP

--- debug.py
+++ debug.py
@@ -4,18 +4,22 @@ pygame.init()
 # Attributes
 BG_COLOR = (0,0,0)
 FONT_COLOR = (255,255,255)
-DEFAULT_X, DEFAULT_Y = (10,10)
+TOPLEFT = (10,10)
 FONT = None  # None for pygame default font
 ANTIALIAS = True
 
-# This method converts the input to a string and displays it
+# This method converts the input list to a string and displays it
 # as a rect at position topleft = (x,y), default included
+# Must input a list (single strings get chopped)
 
-f = pygame.font.Font(FONT,30)
-
-def debug(info, x = DEFAULT_X, y = DEFAULT_Y):    
+def debug(info_list, x = TOPLEFT[0], y = TOPLEFT[1]):
+    f = pygame.font.Font(FONT,30)  
     display_surf = pygame.display.get_surface()
-    debug_surf = f.render(str(info), ANTIALIAS, FONT_COLOR)
-    debug_rect = debug_surf.get_rect(topleft = (x,y))
-    pygame.draw.rect(display_surf, BG_COLOR, debug_rect)
-    display_surf.blit(debug_surf, debug_rect)
+    line_height = pygame.font.Font.get_linesize(f)
+
+    for index, line in enumerate(info_list):
+        debug_surf = f.render(str(line), ANTIALIAS, FONT_COLOR)
+        position = (20, TOPLEFT[1] + index * line_height)
+        debug_rect = debug_surf.get_rect(topleft = position)
+        pygame.draw.rect(display_surf, BG_COLOR, debug_rect)
+        display_surf.blit(debug_surf, debug_rect)
